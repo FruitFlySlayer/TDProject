2a586daf8d486e011725864a9b4472af0bc0f25d
FruitFlySlayer <60361816+FruitFlySlayer@users.noreply.github.com>
2020-11-10 20:15:42 -0800

    AI
    Added AI to the enemies, although it is extremely basic as of right now considering the map is a single straight line.

---
+++ Assets/Endpoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Endpoint : MonoBehaviour
+{
+    public static Transform endpoint;
+
+    private void Awake()
+    {
+        endpoint = gameObject.transform;
+        
+    }
+}
---
+++ Assets/GenericEnemyBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericEnemyBehavior : MonoBehaviour
+{
+    public float speed = 0.01f;
+    public int health = 10;
+    private Transform end;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        end = Endpoint.endpoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = end.position - transform.position;
+        transform.position = Vector3.MoveTowards(transform.position, end.position, speed);
+        if (Vector3.Distance(transform.position, end.position) < 0.1f)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    
+}
